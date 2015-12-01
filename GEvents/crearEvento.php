06e1cc8d3c4d01772372ce9bc49acb3aba4d03d8
Antonio Espinosa <jonnny0@hotmail.com>
2015-12-01 19:22:52 +0100

    crearEvento terminado

--- GEvents/crearEvento.php
+++ GEvents/crearEvento.php
@@ -2,9 +2,10 @@
 
 include_once "dbConnect.php";
 
-//inserto en evento, cojo su id e inserto en organizador
+//inserto en evento, cojo su id e inserto en organizador la pareja usuario-evento
 
-if (isset($_POST['nombre']) && isset($_POST['dscripcion']) && isset($_POST['lugar']) &&
+if (isset($_SESSION['idUsuario']) && isset($_POST['nombre']) &&
+        isset($_POST['dscripcion']) && isset($_POST['lugar']) &&
         isset($_POST['fechaInicio']) && isset($_POST['fechaFin']) && isset($_POST['imagen'])) {
 
     $idUsuario = $_SESSION["idUsuario"];
@@ -18,20 +19,34 @@ if (isset($_POST['nombre']) && isset($_POST['dscripcion']) && isset($_POST['luga
     $conexion = dbConnect();
 
     $sql = "INSERT INTO Evento (nombre, descripcion, lugar, fechaInicio, fechaFin, imagen)
-VALUES ('" + $nombre + "', '" + $descripcion + "', '" + $lugar + "', '" +
+                VALUES ('" + $nombre + "', '" + $descripcion + "', '" + $lugar + "', '" +
             $fechaInicio + "', '" + $fechaFin + "', '" + $imagen + "')";
 
-    // Ejecutamos el código SQL
+
     $resultado = mysql_query($sql, $conexion);
 
     if ($resultado == FALSE) {
         echo '<br>ERROR: No se pudo realizar la operación: ' . $sql . '<br>' . mysql_error();
     } else {
-
-
         $sql = "SELECT id FROM Evento WHERE nombre='" + $nombre + "' AND descripcion='" +
-        $descripcion + "' AND lugar='" + $lugar + "' AND fechaInicio='" +
-        $fechaInicio + "' AND fechaFin='" + $fechaFin + "' AND imagen='" + $imagen + "'";
+                $descripcion + "' AND lugar='" + $lugar + "' AND fechaInicio='" +
+                $fechaInicio + "' AND fechaFin='" + $fechaFin + "' AND imagen='" + $imagen + "'";
+
+        $resultado = mysql_query($sql, $conexion);
+
+        if ($resultado == FALSE) {
+            echo '<br>ERROR: No se pudo realizar la operación: ' . $sql . '<br>' . mysql_error();
+        } else {
+            $registro = mysql_fetch_array($resultado);
+
+            $sql = "INSERT INTO Organizador (usuario, evento) VALUES ('" + $idUsuario + "', '" + $registro['id'] + "')";
+
+            $resultado = mysql_query($sql, $conexion);
+
+            if ($resultado == FALSE) {
+                echo '<br>ERROR: No se pudo realizar la operación: ' . $sql . '<br>' . mysql_error();
+            }
+        }
     }
 
     mysql_close($conexion);
