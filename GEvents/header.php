0000ed8182dfbe4e8ac12ffb4b59f4cd0bfa465c
Louri <louri@correo.ugr.es>
2015-12-02 12:36:23 +0100

    index sin terminar

---
+++ GEvents/header.php
@@ -0,0 +1,40 @@
+<html lang="es">
+<head>
+<meta charset="utf-8">
+<link rel="icon" href="assets/img/favicon.ico" type="image/x-icon">
+<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+<!-- css -->
+<link href="assets/css/bootstrap.min.css" rel="stylesheet"/>
+<link href="assets/css/style.css" rel="stylesheet"/>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="assets/font-awesome/css/font-awesome.min.css" type="text/css">
+</head>
+
+<nav class="navbar navbar-default navbar-fixed-top">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" class="logo" alt="GEvents"></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav navbar-right">
+          <li><a href="#"><i class="fa fa-user usuario"></i>Identifícate</a></li>
+      </ul>
+      <form class="navbar-form navbar-right" role="search">
+        <div class="input-group">
+          <span class="input-group-addon" id="basic-addon1"><i class="fa fa-search"></i></span>
+          <input type="text" class="form-control" placeholder="Buscar">
+        </div>
+       </form>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
---
+++ GEvents/index.php
@@ -0,0 +1,16 @@
+<?php include 'header.php'; ?>
+<body>
+  <section>
+      <h1>Eventos<hr></h1>
+      <article>
+        <div class="contenidoEvento">
+          <h2 class="nombreEvento">Fiestas Lobras 2016</h2>
+          <img class="evento" src="assets/img/evento.png">
+          <p>Lobras(Granada)</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lobortis sagittis interdum. Pellentesque eleifend in nunc eget elementum. Ut efficitur dictum purus, ut suscipit lectus consequat a. Nunc ut risus venenatis, bibendum tortor ut, imperdiet dui. Ut consectetur diam nec leo viverra, vitae consectetur velit cursus. Phasellus eget ligula mattis, semper justo eu, venenatis enim.</p>
+          <p>20/08/2016 - 29/08/2016</p>
+        </div>
+      </article>
+  </section>
+</body>
+<?php include 'footer.php'; ?>
