ab2f0672d6e61152888380fa4701a2278ef0cfca
Louri <louri@correo.ugr.es>
2015-12-03 20:56:41 +0100

    formulario de registro completo

--- GEvents/registro.php
+++ GEvents/registro.php
@@ -2,7 +2,7 @@
 <body>
 <section>
 <article>
-	<form class="form-horizontal" method="POST" action="#" data-toggle="validator" role="form">
+	<form class="form-horizontal" method="POST" action="registrarUsuario.php" data-toggle="validator" role="form">
 			<div class="form-group has-feedback">
 				<label class="col-sm-2 control-label">Nombre de usuario</label>
 				<div class="col-sm-10">
@@ -13,7 +13,7 @@
 		<div class="form-group has-feedback">
 				<label class="col-sm-2 control-label">Email</label>
 				<div class="col-sm-10">
-					<input type="email" pattern="^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$" class="form-control" id="correo" name="correo" placeholder="Email" required>
+					<input type="email" pattern="^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$" class="form-control" id="correo" name="correo" placeholder="ejemplo@ejemplo.com" required>
 				</div>
 				<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 			</div>
@@ -31,6 +31,48 @@
 				</div>
 				<span class="glyphicon form-control-feedback" aria-hidden="true"></span>
 			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Nombre</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="nombre" name="nombre" placeholder="Carlos">
+			</div>
+			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Apellidos</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="apellidos" name="apellidos" placeholder="Fernández">
+			</div>
+			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Dirección</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="direccion" name="direccion" placeholder="Calle Limonar">
+			</div>
+			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Localidad</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="localizacion" name="localizacion" placeholder="Granada">
+			</div>
+			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Teléfono</label>
+				<div class="col-sm-10">
+					<input type="tel" class="form-control" id="telefono" name="telefono" placeholder="612345678">
+			</div>
+			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Fecha de nacimiento</label>
+				<div class="col-sm-10">
+					<input type="date" class="form-control" id="fechaNacimiento" name="fechaNacimiento" placeholder="dd/mm/aaaa">
+			</div>
+			</div>
+		<div class="form-group">
+				<label class="col-sm-2 control-label">Imagen de perfil</label>
+				<div class="col-sm-10">
+					<input type="file" class="form-control" id="imagen" name="imagen" placeholder="dd/mm/aaaa">
+			</div>
+		</div>
 			<div class="form-group">
 				<div class="col-sm-offset-2 col-sm-10">
 					<button type="submit" class="btn btn-default inicioSesion">Regístrate</button>
