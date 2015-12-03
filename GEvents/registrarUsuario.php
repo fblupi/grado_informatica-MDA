e52fe3201c24466c22723afb8c1468148c645a57
Jogua <josegua93@gmail.com>
2015-12-03 19:54:41 +0100

    cambios en registro de usuario

--- GEvents/registrarUsuario.php
+++ GEvents/registrarUsuario.php
@@ -9,50 +9,29 @@ if (!isset($_SESSION['login'])) {//Si no se puede acceder a $_SESSION['login'] e
 $login = $_POST['login'];
 $correo = $_POST['correo'];
 $pass = $_POST['pass'];
-$nombre = "";
-$apellidos = "";
-$direccion = "";
-$telefono = "";
-$localizacion = "";
-$fechaNacimiento = null;
+$nombre = $_POST['nombre'];
+$apellidos = $_POST['apellidos'];
+$direccion = $_POST['direccion'];
+$telefono = $_POST['telefono'];
+$localizacion = $_POST['localizacion'];
+$fechaNacimiento = $_POST['fechaNacimiento'];
 $imagen = "assets/img/usuario.png";
-
-if (isset($_POST['nombre'])) {
-	$nombre = $_POST['nombre'];
-}
-if (isset($_POST['apellidos'])) {
-	$apellidos = $_POST['apellidos'];
-}
-if (isset($_POST['direccion'])) {
-	$direccion = $_POST['direccion'];
-}
-if (isset($_POST['telefono'])) {
-	$telefono = $_POST['telefono'];
-}
-if (isset($_POST['localizacion'])) {
-	$localizacion = $_POST['localizacion'];
-}
-if (isset($_POST['fechaNacimiento'])) {
-	$fechaNacimiento = $_POST['fechaNacimiento'];
-}
-if(isset($_FILES['imagen'])){
-	if ($_FILES['imagen']["error"] > 0) {
-        salir("Ha ocurrido un error en la carga de la imagen", -2);
-    } else {
-        $permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
-        $limite_kb = 2048;
-        if (in_array($_FILES['imagen']['type'], $permitidos) && $_FILES['imagen']['size'] <= $limite_kb * 1024) {
-            $carpeta = "./images/users";
-            if (!is_dir($carpeta)) {
-                mkdir($carpeta);
-            }
-            $nombre_archivo = $login . ' - ' . $_FILES['imagen']['name'];
-            $ruta = $carpeta . "/" . $nombre_archivo;
-            if (!file_exists($ruta)) {
-                $resultado_subida = @move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta);
-                if ($resultado_subida) {
-                    $imagen = $ruta;
-                }
+if ($_FILES['imagen']["error"] > 0) {
+    salir("Ha ocurrido un error en la carga de la imagen", -2);
+} else {
+    $permitidos = array("image/jpg", "image/jpeg", "image/gif", "image/png");
+    $limite_kb = 2048;
+    if (in_array($_FILES['imagen']['type'], $permitidos) && $_FILES['imagen']['size'] <= $limite_kb * 1024) {
+        $carpeta = "./images/users";
+        if (!is_dir($carpeta)) {
+            mkdir($carpeta);
+        }
+        $nombre_archivo = $login . ' - ' . $_FILES['imagen']['name'];
+        $ruta = $carpeta . "/" . $nombre_archivo;
+        if (!file_exists($ruta)) {
+            $resultado_subida = @move_uploaded_file($_FILES['imagen']['tmp_name'], $ruta);
+            if ($resultado_subida) {
+                $imagen = $ruta;
             }
         }
     }
