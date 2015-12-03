c67ed8ec8ad2649bf7c614682fb6af4a2f3720b4
Louri <louri@correo.ugr.es>
2015-12-03 19:11:43 +0100

    recordar contraseña

---
+++ GEvents/recordarPass.php
@@ -0,0 +1,29 @@
+<?php include 'header.php'; ?>
+<body>
+<section>
+<article>
+	<form class="form-horizontal" method="POST" action="#">
+			<div class="form-group">
+				<label class="col-sm-2 control-label">Nombre de usuario</label>
+				<div class="col-sm-10">
+					<input type="text" class="form-control" id="login" name="login" placeholder="Nombre de usuario">
+				</div>
+			</div>
+			<div class="form-group">
+				<label class="col-sm-2 control-label">Email</label>
+				<div class="col-sm-10">
+					<input type="email" class="form-control" id="correo" name="correo" placeholder="Email">
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-offset-2 col-sm-10">
+					<button type="submit" class="btn btn-default inicioSesion">Recordar Contraseña</button>
+				</div>
+				<small class="izquierda"><a href="registro.php">¿No estás registrado?</a></small>
+
+			</div>
+		</form>
+</article>
+</section>
+</body>
+<?php include 'footer.php'; ?>
