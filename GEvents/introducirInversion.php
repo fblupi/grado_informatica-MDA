46449ebce89c6231d53838c90e5b008becf983fa
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2015-12-04 20:47:41 +0100

    Script de introducir inversión

---
+++ GEvents/introducirInversion.php
@@ -0,0 +1,44 @@
+<?php
+
+include_once "dbConnect.php";
+
+$concepto = $_POST['concepto'];
+$descripcion = $_POST['descripcion'];
+$precioCompra = $_POST['precioCompra']; // importe en "Cuenta"
+$cantidad = $_POST['cantidad'];
+$precioVenta = $_POST['precioVenta'];
+$evento = $_GET['evento'];
+$tipo = "Gasto";
+$fecha = date("d/m/Y");
+
+if (!empty($concepto) && !empty($descripcion) && !empty($precioCompra) &&!empty($cantidad) &&
+    !empty($precioVenta) && !empty($evento) && !empty($tipo) && !empty($fecha)) {
+  $conexion = dbConnect();
+  $sqlCuenta = "INSERT INTO Cuenta (concepto, importe, descripcion, cantidad, evento, tipo, fecha)
+                VALUES ('" + $concepto + "', '" + ($importe * (-1)) + "', '" + $descripcion + "', '" +
+                $cantidad + "', '" + $evento + "', '" + $tipo + "', '" + $fecha + "');";
+  $sqlProducto = "INSERT INTO Producto (nombre, precioCompra, precioVenta, cantidad, evento)
+                  VALUES ('" + $concepto + "', '" + $precioCompra + "', '" + $precioVenta + "', '" + $cantidad + "', '" + $evento + "');";
+  if ($sqlCuenta) {
+    if ($sqlProducto) {
+      salir("La inversión se ha introducido correctamente", 0);
+    } else {
+      salir("Error introduciendo la inversión. No se pudo realizar la operación: " . $sqlProducto . "<br>" . mysql_error(), -1);
+    }
+  } else {
+    salir("Error introduciendo la inversión. No se pudo realizar la operación: " . $sqlCuenta . "<br>" . mysql_error(), -1);
+  }
+} else {
+  salir("No se ha podido introducir una inversión con esos datos", -1);
+}
+
+function salir($str, $code) {
+  mysqli_close($conexion);
+  echo '<script>
+    alert("' . $str . '");
+    location.href= " ' . $_SERVER['HTTP_REFERER'] . '";
+  </script>';
+  return $code;
+}
+
+?>
