992e1c461557065ad0f2e7b3cd362686d28fadf1
Antonio Espinosa <jonnny0@hotmail.com>
2015-12-04 19:31:21 +0100

    IntroducirGasto.php terminado

---
+++ GEvents/introducirGasto.php
@@ -0,0 +1,49 @@
+<?php
+
+include_once "dbConnect.php";
+
+//introducir un registro en Cuenta sobre un evento con importe negativo (gasto)
+
+$concepto = $_POST["concepto"];
+$importe = $_POST["importe"];
+$descripcion = $_POST["descripcion"];
+$cantidad = $_POST["cantidad"];
+$evento = $_POST["evento"];
+$tipo = $_POST['tipo'];
+$fecha = $_POST['fecha'];
+
+if (!empty($concepto) && !empty($importe) && !empty($descripcion) &&
+        !empty($cantidad) && !empty($evento) && !empty($tipo) && !empty($fecha)) {
+
+    //realizo la conexión
+    $conexion = dbConnect();
+
+    //inserto los datos introducidos
+    $sql = "INSERT INTO Cuenta (concepto, importe, descripcion, cantidad, evento, tipo, fecha)
+                VALUES ('" + $concepto + "', '" + ($importe * (-1)) + "', '" + $descripcion + "', '" +
+            $cantidad + "', '" + $evento + "', '" + $tipo + "', '" + $fecha + "')";
+
+    //almaceno en $resultado
+    $resultado = mysqli_query($conexion, $sql);
+
+    //si hay error al insertar el gasto, muestro el error y salgo
+    if (!$resultado) {
+        salir('ERROR: No se pudo realizar la operación: ' . $sql . '<br>' . mysql_error(), -1);
+
+    //si no hay error, muestro que se ha insertado correctamente
+    } else {
+        salir('Se insertó el gasto correctamente', 0);        
+    }
+}
+
+//cierra la conexión, muestra el resultado y devuelve el control a la página desde la que se referencia
+function salir($str, $code) {
+    mysqli_close($conexion);
+    echo '<script>
+            alert("' . $str . '");
+            location.href= " ' . $_SERVER['HTTP_REFERER'] . '";
+        </script>';
+    return $code;
+}
+
+?>
