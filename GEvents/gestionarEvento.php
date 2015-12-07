9af58785f7e15eb12bfe0d05b3048ceee7cf1a15
Louri <louri@correo.ugr.es>
2015-12-07 13:48:21 +0100

    panel de control de eventos
    Añadida interfaz del panel de control de eventos

---
+++ GEvents/gestionarEvento.php
@@ -0,0 +1,42 @@
+<?php include 'header.php'; 
+if(isset($_GET['i'])){
+	$idEvento = $_GET['i'];
+}
+?>
+<body>
+  <section>
+      <article>
+       <?php 
+				include 'libs/myLib.php';
+				$conn = dbConnect();
+				
+				$sql = "SELECT nombre FROM Evento WHERE id = '$idEvento';";
+				$resultado = mysqli_query($conn, $sql);
+				while($eventos = mysqli_fetch_assoc($resultado)){
+					echo '<h1>';
+					echo 'Panel de control <small class="tituloGestionEvento">('.$eventos['nombre'].')</small>';
+					echo '<hr></h1>';
+					echo '<h2>Personal</h2>';
+					echo '<a href="" class="btn btn-default btnGestionEvento">';
+					echo 'Gestionar organizadores';
+					echo '</a>';
+					echo '<h2>Contabilidad</h2>';
+					echo '<a href="" class="btn btn-default btnGestionEvento">';
+					echo 'Introducir gasto';
+					echo '</a>';
+					echo '<a href="" class="btn btn-default btnGestionEvento">';
+					echo 'Introducir inversión';
+					echo '</a>';
+					echo '<a href="" class="btn btn-default btnGestionEvento">';
+					echo 'Introducir donación';
+					echo '</a>';
+					echo '<a href="" class="btn btn-default btnGestionEvento">';
+					echo 'Consultar balance';
+					echo '</a>';
+				}
+				
+				?>
+      </article>
+  </section>
+</body>
+<?php include 'footer.php'; ?>
