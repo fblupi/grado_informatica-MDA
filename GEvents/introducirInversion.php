d501e60a9685d93987e712ac6104e2bf8f582ba3
Louri <louri@correo.ugr.es>
2015-12-22 11:19:44 +0100

    Interfaz de introducir inversión

--- GEvents/introducirInversion.php
+++ GEvents/introducirInversion.php
@@ -6,23 +6,27 @@ if(!isset($_SESSION['login'])){
 <body>
 <section id="divInicioSesion" class="divInicioSesion">
 <article>
-	<form class="form-signin" method="POST" id="formularioIntroducirGasto" name="formularioIntroducirGasto" action="" data-toggle="validator" enctype="multipart/form-data">
+	<form class="form-signin" method="POST" id="formularioIntroducirInversion" name="formularioIntroducirInversion" action="" data-toggle="validator" enctype="multipart/form-data">
 			<div class="form-group has-feedback">
 				<label>Concepto</label>
-				<input type="text" class="form-control" id="concepto" name="concepto" placeholder="Comprar..." maxlength="45" required>
+				<input type="text" class="form-control" id="concepto" name="concepto" maxlength="45" required>
 			</div>
 		<div class="form-group">
 				<label>Descripción</label>
 				<textarea class="form-control" id="descripcion" name="descripcion" rows="5" placeholder="Breve descripción del gasto" required></textarea>
 			</div>
 			<div class="form-group">
-				<label>Importe (€)</label>
-				<input type="number" class="form-control" id="importe" name="importe" required>
+				<label>Precio de compra (€)</label>
+				<input type="text" class="form-control" id="precioCompra" name="precioCompra" required>
 			</div>
       <div class="form-group">
         <label>Cantidad</label>
-        <input type="number" class="form-control" id="importe" name="importe" required>
+        <input type="number" class="form-control" id="cantidad" name="cantidad" required>
       </div>
+      <div class="form-group">
+				<label>Precio de venta (€)</label>
+				<input type="text" class="form-control" id="precioVenta" name="precioVenta" required>
+			</div>
 			<div class="form-group">
 					<button type="submit" class="btn btn-success inicioSesion btnCrear">Introducir</button>
 					<a type="button" class="btn btn-primary inicioSesion btnVolver" onClick="history.go(-1);return true;">Volver</a>
