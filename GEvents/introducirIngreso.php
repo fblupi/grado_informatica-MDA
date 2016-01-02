d22c667ade06a5824b05bf65fbba7ef7bde1e7b7
Amanda Fernández Piedra <louri@correo.ugr.es>
2016-01-02 10:46:18 +0100

    interfaz introducir ingreso

---
+++ GEvents/introducirIngreso.php
@@ -0,0 +1,36 @@
+<?php include 'header.php';
+if(!isset($_SESSION['login'])){
+	echo '<script>location.href="signin.php";</script>';
+}
+?>
+<body>
+<section>
+  <h1>Introducir ingreso<hr></h1>
+<article>
+	<form class="form-signin" method="POST" id="formularioIntroducirIngreso" name="formularioIntroducirIngreso" action="scripts/introducirIngreso.php" data-toggle="validator" enctype="multipart/form-data">
+			<div class="form-group has-feedback">
+				<label>Concepto</label>
+				<input type="text" class="form-control" id="concepto" name="concepto" maxlength="45" required>
+			</div>
+		<div class="form-group">
+				<label>Descripción</label>
+				<textarea class="form-control" id="descripcion" name="descripcion" rows="5" placeholder="Breve descripción del gasto" required></textarea>
+			</div>
+			<div class="form-group">
+				<label>Importe (€)</label>
+				<input type="text" class="form-control" id="precioCompra" name="precioCompra" required>
+			</div>
+      <div class="form-group">
+        <label>Cantidad</label>
+        <input type="number" class="form-control" id="cantidad" name="cantidad" required>
+      </div>
+			<div class="form-group">
+					<button type="submit" class="btn btn-success inicioSesion btnCrear">Introducir</button>
+					<a type="button" class="btn btn-primary inicioSesion btnVolver" onClick="history.go(-1);return true;">Volver</a>
+			</div>
+		</form>
+</article>
+</section>
+</body>
+
+<?php include 'footer.php'; ?>
