663918de0de6a7a70994d22e40439bee448c7549
Amanda Fernández Piedra <louri@correo.ugr.es>
2016-01-02 11:51:08 +0100

    mostrar balance detallado

--- GEvents/consultarBalance.php
+++ GEvents/consultarBalance.php
@@ -13,12 +13,12 @@ if(!isset($_SESSION['login'])){
         		//Realizo la conexión si tenemos todos los datos necesarios
             include_once "libs/myLib.php";
             $conexion = dbConnect();
-        		$gasto="SELECT SUM(importe) AS gasto FROM cuenta WHERE tipo='gasto' AND evento='$id_evento';";
+        		$gasto="SELECT SUM(importe) AS gasto FROM cuenta WHERE tipo='0' AND evento='$id_evento';";
         		$resultadogasto = mysqli_query($conexion, $gasto);
-            $gasto = mysqli_fetch_assoc($resultadogasto);
-        		$beneficio="SELECT SUM(importe) AS beneficio FROM cuenta WHERE tipo='beneficio' AND evento='$id_evento';";
+            $gastos = mysqli_fetch_assoc($resultadogasto);
+        		$beneficio="SELECT SUM(importe) AS beneficio FROM cuenta WHERE tipo='1' AND evento='$id_evento';";
         		$resultadobenefi = mysqli_query($conexion, $beneficio);
-            $beneficio = mysqli_fetch_assoc($resultadobenefi);
+            $beneficios = mysqli_fetch_assoc($resultadobenefi);
         		?>
             <div class="row">
             <div class="col-md-4 col-lg-4">
@@ -26,30 +26,31 @@ if(!isset($_SESSION['login'])){
         		<table class="table table-hover">
         		<tr>
         		<td>Gastos</td><td><?php
-            if($gasto['gasto']==NULL){
+            if($gastos['gasto']==NULL){
               echo '0';
             }else{
-              echo $gasto['gasto'];
+              echo $gastos['gasto'];
             }
             ?> €</td>
         		</tr>
         		<tr>
         		<td>Ingresos</td><td><?php
-            if($beneficio['beneficio']==NULL){
+            if($beneficios['beneficio']==NULL){
               echo '0';
             }else{
-              echo $beneficio['beneficio'];
+              echo $beneficios['beneficio'];
             }
             ?> €</td>
         		</tr>
-        		<tr class="balance" <?php if($beneficio['beneficio']-$gasto['gasto'] < 0){
+        		<tr class="balance" <?php if($beneficios['beneficio']-$gastos['gasto'] < 0){
               echo 'style="color: red;"';
             }else{
               echo 'style="color: green;"';
             } ?>>
-        		<td>Balance</td><td><?php echo $beneficio['beneficio']-$gasto['gasto'] ?> €</td>
+        		<td>Balance</td><td><?php echo $beneficios['beneficio']-$gastos['gasto'] ?> €</td>
         		</tr>
         		</table>
+						<a href="mostrarBalanceDetallado.php?i=<?php echo $id_evento; ?>" class="verBalanceDetallado">Ver balance detallado</a>
             </div>
             <div class="col-md-4 col-lg-4">
             </div>
@@ -58,28 +59,28 @@ if(!isset($_SESSION['login'])){
         		<table class="table table-hover">
         		<tr>
             <td>Gastos</td><td><?php
-            if($gasto['gasto']==NULL){
+            if($gastos['gasto']==NULL){
               echo '0';
             }else{
-              echo $gasto['gasto'];
+              echo $gastos['gasto'];
             }
             ?> €</td>
             </tr>
         		<tr>
             <td>Ingresos estimados <i class="fa fa-info-circle infoBalancei"></i><div class="infoBalance"><span>Ingresos estimados si se venden todos los productos</span></div></td><td><?php
-            if($beneficio['beneficio']==NULL){
+            if($beneficios['beneficio']==NULL){
               echo '0';
             }else{
-              echo $beneficio['beneficio'];
+              echo $beneficios['beneficio'];
             }
             ?> €</td>
         		</tr>
-            <tr class="balance" <?php if($beneficio['beneficio']-$gasto['gasto'] < 0){
+            <tr class="balance" <?php if($beneficios['beneficio']-$gastos['gasto'] < 0){
               echo 'style="color: red;"';
             }else{
               echo 'style="color: green;"';
             } ?>>
-        		<td>Balance</td><td><?php echo $beneficio['beneficio']-$gasto['gasto'] ?> €</td>
+        		<td>Balance</td><td><?php echo $beneficios['beneficio']-$gastos['gasto'] ?> €</td>
         		</tr>
         		</table>
             </div>
@@ -89,7 +90,11 @@ if(!isset($_SESSION['login'])){
         		mysqli_close($conexion);
         	}//
         ?>
-			<a type="button" class="btn btn-primary" onClick="history.go(-1);return true;">Volver</a>
+			<div class="row">
+			<div class="col-md-4 col-lg-4">
+				<a type="button" class="btn btn-primary" onClick="history.go(-1);return true;">Volver</a>
+			</div>
+			</div>
       </article>
   </section>
 </body>
---
+++ GEvents/mostrarBalanceDetallado.php
@@ -0,0 +1,85 @@
+<?php include 'header.php';
+if(!isset($_SESSION['login'])){
+	echo '<script>location.href="signin.php";</script>';
+}
+?>
+<body>
+  <section>
+      <h1>Balance detallado<hr></h1>
+      <article>
+      <div class="row">
+        <table class="table table-hover">
+          <thead>
+            <th>Concepto</th>
+            <th>Fecha</th>
+            <th>Gasto (€)</th>
+            <th>Ingreso (€)</th>
+            <th>Balance (€)</th>
+          </thead>
+          <?php
+          include_once 'libs/myLib.php';
+          $conn = dbConnect();
+          $idEvento = $_GET['i'];
+          $sql = "SELECT * FROM cuenta WHERE evento = $idEvento ORDER BY fecha DESC;";
+          $resultado = mysqli_query($conn, $sql);
+
+          while($cuenta = mysqli_fetch_assoc($resultado)){
+            echo '<tr>';
+            echo '<td>';
+            echo $cuenta['concepto'];
+            echo '</td>';
+            echo '<td>';
+            echo date('j F, Y', strtotime($cuenta['fecha']));
+            echo '</td>';
+            if($cuenta['tipo']==0){
+              echo '<td>';
+              echo $cuenta['importe'];
+              echo '</td>';
+              echo '<td></td>';
+              echo '<td></td>';
+            }else{
+              echo '<td></td>';
+              echo '<td>';
+              echo $cuenta['importe'];
+              echo '</td>';
+              echo '<td></td>';
+            }
+            echo '</tr>';
+          }
+
+          $gasto="SELECT SUM(importe) AS gasto FROM cuenta WHERE tipo='0' AND evento='$idEvento';";
+          $resultadogasto = mysqli_query($conn, $gasto);
+          $gastos = mysqli_fetch_assoc($resultadogasto);
+          $beneficio="SELECT SUM(importe) AS beneficio FROM cuenta WHERE tipo='1' AND evento='$idEvento';";
+          $resultadobenefi = mysqli_query($conn, $beneficio);
+          $beneficios = mysqli_fetch_assoc($resultadobenefi);
+
+          echo '<tr class="totalBalanceDetallado">';
+          echo '<td colspan="2">Total</td>';
+          echo '<td>';
+          echo $gastos['gasto'];
+          echo '</td>';
+          echo '<td>';
+          echo $beneficios['beneficio'];
+          echo '</td>';
+          echo '<td ';
+          if($beneficios['beneficio']-$gastos['gasto'] < 0){
+            echo 'style="color: red;"';
+          }else{
+            echo 'style="color: green;"';
+          }
+          echo '>';
+          echo $beneficios['beneficio']-$gastos['gasto'];
+          echo '</td>';
+          ?>
+        </table>
+      </div>
+			<div class="row">
+			<div class="col-md-4 col-lg-4">
+				<a type="button" class="btn btn-primary" onClick="history.go(-1);return true;">Volver</a>
+			</div>
+			</div>
+      </article>
+  </section>
+</body>
+<?php include 'footer.php'; ?>
