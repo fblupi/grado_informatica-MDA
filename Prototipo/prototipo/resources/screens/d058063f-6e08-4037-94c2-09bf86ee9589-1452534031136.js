b007f7d1f265c7549a6c1fa80b9aaf40a1e408c4
Francisco Javier Bolívar Lupiáñez <fboli94@gmail.com>
2016-01-11 20:24:38 +0100

    Nuevos bocetos

---
+++ Prototipo/prototipo/resources/screens/d058063f-6e08-4037-94c2-09bf86ee9589-1452534031136.js
@@ -0,0 +1,54 @@
+jQuery("#simulation")
+  .on("click", ".s-d058063f-6e08-4037-94c2-09bf86ee9589 .click", function(event, data) {
+    var jEvent, jFirer, cases;
+    if(data === undefined) { data = event; }
+    jEvent = jimEvent(event);
+    jFirer = jEvent.getEventFirer();
+    if(jFirer.is("#s-Button_1")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/38dc8c72-b96c-425e-9592-a3dbec0fc9a3"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    } else if(jFirer.is("#s-Button_3")) {
+      cases = [
+        {
+          "blocks": [
+            {
+              "actions": [
+                {
+                  "action": "jimNavigation",
+                  "parameter": {
+                    "target": "screens/4d225cc0-48c8-47dd-9665-a4b913f57657"
+                  },
+                  "exectype": "serial",
+                  "delay": 0
+                }
+              ]
+            }
+          ],
+          "exectype": "serial",
+          "delay": 0
+        }
+      ];
+      event.data = data;
+      jEvent.launchCases(cases);
+    }
+  });
